14e11db1eae780fe8191aefbb141ff686df331ab
Mike Kalan <mkalan@tamu.edu>
2015-11-16 04:24:38 +0000

    New changes from master to mike

--- rankine.py
+++ rankine.py
@@ -7,7 +7,14 @@ matplotlib.use('Agg') # to get matplotlib to save figures to a file instead of u
 import matplotlib.pyplot as plt
 #import os           # for file system utilities
 import sys
+<<<<<<< Updated upstream
 from prettytable import PrettyTable #for output formatting
+=======
+from prettytable import PrettyTable, MSWORD_FRIENDLY, PLAIN_COLUMNS #for output formatting
+import csv
+
+import CoolProp.CoolProp as CP
+>>>>>>> Stashed changes
 
 ######################################
 
@@ -16,6 +23,7 @@ def main():
     props = define_inputs()
     print(props)
     # begin computing processess for rankine cycle
+<<<<<<< Updated upstream
     (cyc_props,p_list,s_list) = compute_cycle(props)
     print('Done!')
 
@@ -127,6 +135,26 @@ def main():
     #   if not os.access(filename,os.W_OK): # check to see if it is not writable
     #     os.fchmod(filename,stat.S_IWOTH) # if not writable, then make it writable
     plt.savefig(filename) # save figure to directory
+=======
+    rankine = compute_cycle(props)
+    cyc_props = {}
+    cyc_props['wnet'] = rankine.wnet
+    cyc_props['qnet'] = rankine.qnet
+    cyc_props['thermal_eff'] = rankine.thermal_eff
+    cyc_props['bwr'] = rankine.bwr
+    s_list = rankine.get_states()
+    p_list = rankine.get_procs()
+
+#     initialize geothermal cycle using defaults
+#     geotherm = thermo.Geotherm()
+
+    # print output to screen
+    print_output_to_screen(cyc_props,p_list,s_list,props,s_list[0].cycle.dead)
+
+#     compute plant efficiencies
+#     plant = compute_plant(rankine,geotherm)
+
+>>>>>>> Stashed changes
     return
 
 
@@ -288,6 +316,7 @@ def compute_cycle(props):
     process_list = [turb,cond,pump,boil]
 
     # Define cycle properties
+<<<<<<< Updated upstream
     wnet = turb.work + pump.work
     qnet = boil.heat + cond.heat
     # Find thermal efficiency for cycle
@@ -301,6 +330,173 @@ def compute_cycle(props):
     cyc_props['bwr'] = bwr
 
     return (cyc_props, process_list, state_list)
+=======
+    cyc.wnet = turb.work + pump.work
+    cyc.qnet = boil.heat + cond.heat
+    cyc.thermal_eff = cyc.wnet / boil.heat
+    cyc.bwr = -pump.work / turb.work
+
+    return cyc
+
+def print_output_to_screen(cyc_props,p_list,s_list,props,dead):
+    print_user_values(props)
+    print_state_table(s_list,dead)
+    print_process_table(cyc_props,p_list)
+    print_exergy_table(p_list)
+    print_cycle_values(cyc_props)
+    create_plot(p_list,s_list)
+    write_csv(p_list,s_list)
+    return
+
+def print_user_values(props):
+    # print values to screen
+    print('\nUser entered values\n-------------------')
+    print('Working Fluid: '+props["fluid"])
+    print('Low Pressure:  {:>3.3f} MPa'.format(props["p_lo"]))
+    print('High Pressure: {:>3.3f} MPa'.format(props["p_hi"]))
+    print('Isentropic Turbine Efficiency: {:>2.1f}%'.format(props["turb_eff"]*100))
+    print('Isentropic Pump Efficiency:    {:>2.1f}%\n'.format(props["pump_eff"]*100))
+    return
+
+def print_state_table(s_list,dead):
+    headers = ['State','Press (MPa)','Temp (deg C)','Enthalpy (kJ/kg)','Entropy (kJ/kg.K)','Flow Exergy (kJ/kg)','Quality']
+    t = PrettyTable(headers)
+    for item in headers[1:6]:
+        t.align[item] = 'r'
+    for item in headers[2:4]:
+        t.float_format[item] = '4.2'
+    t.float_format[headers[1]] = '4.3'
+    t.float_format[headers[4]] = '6.5'
+    t.float_format[headers[5]] = '4.2'
+    t.float_format[headers[6]] = '0.2'
+    t.padding_width = 1
+    s_list.append(dead)
+    for item in s_list:
+        print(item.name)
+        t.add_row([item.name,item.p/1000000,item.T-273,item.h/1000,item.s/1000,item.ef/1000,item.x])
+    print(t,'\n')
+    return
+
+def print_process_table(cyc_props,p_list):
+    headers = ['Process','States','Heat (kJ/kg)','Work (kJ/kg)']
+    t = PrettyTable(headers)
+    #t.set_style(MSWORD_FRIENDLY)
+    for item in headers[2:]:
+        t.align[item] = 'r'
+        t.float_format[item] = '5.1'
+    for p in p_list:
+        t.add_row([p.name,p.state_in.name+' -> '+p.state_out.name,p.heat/1000,p.work/1000])
+    t.add_row(['Net','cycle',cyc_props["qnet"]/1000,cyc_props["wnet"]/1000])
+    print(t, '\n')
+    return
+
+def print_exergy_table(p_list):
+    headers = ['Process','States','Exergy In (kJ/kg)','Exergy Out (kJ/kg)','Delta Ef (kJ/kg)','Exergy Dest. (kJ/kg)','Exergetic Eff.']
+    t = PrettyTable(headers)
+    #t.set_style(PLAIN_COLUMNS)
+    for item in headers[2:6]:
+        t.align[item] = 'r'
+        t.float_format[item] = '5.1'
+    ex_totals = [0,0,0,0]
+    for p in p_list:
+        row = [p.name,p.state_in.name+' -> '+p.state_out.name,p.ex_in/1000,p.ex_out/1000,p.delta_ef/1000,p.ex_d/1000,'{:.1%}'.format(p.ex_eff)]
+        t.add_row(row)
+        # calculate exergy totals
+        idx = 0
+        for i in row[2:6]:
+            ex_totals[idx] += i
+            idx += 1
+    # print net exergy row
+    row = ['Net','']
+    for i in ex_totals:
+        row.append(i)
+    # cycle exergetic efficiency
+    cyc_ex_eff = ex_totals[1]/ex_totals[0]  # (total ex_out)/(total ex_in)
+    row.append('{:.1%}'.format(cyc_ex_eff))
+    t.add_row(row)
+    print(t, '\n')
+    return
+
+def print_cycle_values(cyc_props):
+    print('\nCycle Values \n------------ ')
+    print('thermal efficiency = {:2.1f}%'.format(cyc_props["thermal_eff"]*100))
+    print('back work ratio = {:.3f}'.format(cyc_props["bwr"]))
+    return
+
+#write our output data to a csv file
+def write_csv(p_list,s_list):
+  file = open('test.csv' , 'w')
+  a = csv.writer(file)
+
+  data = ['4','4']
+  a.writerows(data)
+  file.close()
+  return
+
+def create_plot(p_list,s_list):
+    # unpack states
+    st_1 = s_list[0]
+    st_2s = s_list[1]
+    st_2 = s_list[2]
+    st_3 = s_list[3]
+    st_4s = s_list[4]
+    st_4 = s_list[5]
+    st_4b = s_list[6]
+
+    # unpack processes
+    turb = p_list[0]
+    cond = p_list[1]
+    pump = p_list[2]
+    boil = p_list[3]
+
+#    (spts,tpts) = get_sat_dome(cyc.fluid)
+
+    # note: use h4, s4 to fix the state to find T4
+    T_pts = [st_1.T, st_2s.T, st_2.T, st_2s.T, st_3.T, st_4s.T, st_4b.T, st_1.T] # solid lines
+    s_pts = [st_1.s, st_2s.s, st_2.s, st_2s.s, st_3.s, st_4s.s, st_4b.s, st_1.s]
+
+    s_dash_12 = [st_1.s, st_2.s]
+    T_dash_12 = [st_1.T, st_2.T]
+    s_dash_34 = [st_3.s, st_4.s]
+    T_dash_34 = [st_3.T, st_4.T]
+
+    # Draw T-s plot
+    plt.clf()
+    plt.plot(s_pts,T_pts,'b') #,sfsat_pts,Tsat_pts,'g--',sgsat_pts,Tsat_pts,'g--')
+    plt.plot(s_dash_12,T_dash_12,'b--',s_dash_34,T_dash_34,'b--')
+    plt.annotate("2s.", xy = (s_pts[1],T_pts[1]) , xytext = (s_pts[1] + 2,T_pts[1]+25 ), arrowprops=dict(facecolor = 'blue', shrink=0.05),)
+    plt.annotate("2.", xy = (s_pts[2],T_pts[2]) , xytext = (s_pts[2] + 2,T_pts[2]+25 ), arrowprops=dict(facecolor = 'black', shrink=0.05),)
+    plt.annotate("1.", xy = (s_pts[0],T_pts[0]) , xytext = (s_pts[0] + 2,T_pts[0]+25 ), arrowprops=dict(facecolor = 'black', shrink=0.05),)
+    plt.annotate("3.", xy = (s_pts[4],T_pts[4]) , xytext = (s_pts[4] + 2,T_pts[4]+25 ), arrowprops=dict(facecolor = 'black', shrink=0.05),)
+    plt.suptitle("Rankine Cycle T-s Diagram")
+    plt.xlabel("Entropy (J/kg*K)")
+    plt.ylabel("Temperature (deg K)")
+    # Save plot
+    filename = 'ts_plot.png'
+    plt.savefig(filename) # save figure to directory
+    return
+
+def compute_plant(rank,geo):
+    ''' Compute and return plant object from rankine cycle and geothermal cycle objects '''
+    pass
+
+def get_sat_dome(fluid):
+    pass
+#     smin = ?
+#     smax = ?
+#     step = ?
+#     quality = 0
+#     tpts = []
+#     spts = []
+#     crit_pt = thermo.State(None,fluid, critical) point?asdlkfjasd;lkf100 #something
+#     for s in range(s_min:step:s_max):
+#         if s > crit_pt.s:
+#             quality = 1
+#         T = CP.PropsSI('T','S',s,'Q',quality,fluid)
+#         spts.append(s)
+#         tpts.append(T-273) # save in celcius
+#     return spts,tpts
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
   main()
