560427323851654f2c03d0dc27ba713d0692ae83
Sam Friedman <samfriedman@tamu.edu>
2015-12-01 17:26:42 +0000

    fix Butane spelling error

--- input_rankine.py
+++ input_rankine.py
@@ -81,7 +81,7 @@ def select_fluid(props):
                       "R236EA",
                       "CarbonDioxide",
                       "n-Pentane",
-                      "IsoButene"
+                      "IsoButane"
                       "n-Butane",
                       "Ammonia"]
         for i in range(len(fluid_list)):
